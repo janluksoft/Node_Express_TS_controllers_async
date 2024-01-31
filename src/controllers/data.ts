33aab4775f2810fb4c6f95aa777fe6973e79a52b
janluksoft <janluksoft@interia.pl>
2024-01-31 15:46:03 +0100

    added table

--- src/controllers/data.ts
+++ src/controllers/data.ts
@@ -46,4 +46,43 @@ export class CDataTab {
         this.aCountLog += xnAdd;
         return(this.aCountLog);
     };
+
+    public getTable3(): any {
+        const tnames =  [
+            {name: 'Emma',sex: 'woman', age: 25},
+            {name: 'Amelia',sex: 'woman', age: 26},
+            {name: 'Isabella',sex: 'woman', age: 19},
+            {name: 'Helena',sex: 'woman', age: 24},
+            {name: 'Elizabeth',sex: 'woman', age: 19},
+            {name: 'Aleksandra',sex: 'woman', age: 20},
+            {name: 'Veronica',sex: 'woman', age: 25},
+            {name: 'Susan',sex: 'woman', age: 20},
+            {name: 'Peter',sex: 'man', age: 25},
+            {name: 'Mark',sex: 'man', age: 28},
+            {name: 'Dawid',sex: 'man', age: 31},
+            {name: 'Gregory',sex: 'man', age: 33},
+            {name: 'Tom',sex: 'man', age: 34},
+            {name: 'Olaf',sex: 'man', age: 36},
+            {name: 'Hans',sex: 'man', age: 31},
+            {name: 'Adam',sex: 'man', age: 27},
+            {name: 'Emma',sex: 'woman', age: 25},
+            {name: 'Amelia',sex: 'woman', age: 26},
+            {name: 'Isabella',sex: 'woman', age: 19},
+            {name: 'Helena',sex: 'woman', age: 24},
+            {name: 'Elizabeth',sex: 'woman', age: 19},
+            {name: 'Aleksandra',sex: 'woman', age: 20},
+            {name: 'Veronica',sex: 'woman', age: 25},
+            {name: 'Susan',sex: 'woman', age: 20},
+            {name: 'Peter',sex: 'man', age: 25},
+            {name: 'Mark',sex: 'man', age: 28},
+            {name: 'Dawid',sex: 'man', age: 31},
+            {name: 'Gregory',sex: 'man', age: 33},
+            {name: 'Tom',sex: 'man', age: 34},
+            {name: 'Olaf',sex: 'man', age: 36},
+            {name: 'Hans',sex: 'man', age: 31},
+            {name: 'Adam',sex: 'man', age: 27}
+            ];
+        return(tnames);
+    }
+
 }
